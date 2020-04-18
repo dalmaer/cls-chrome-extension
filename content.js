98bf68c195d1b5767ebf371bbbce3b698c847358
Dion Almaer <dion@almaer.com>
2020-04-17 19:12:11 -0600

    guard against double inclusion of the content script

--- content.js
+++ content.js
@@ -1,17 +1,20 @@
-// Store the current layout shift score for the page.
-let cls = 0;
+// guard against double-inclusion
+if (typeof po == "undefined") {
+  // Store the current layout shift score for the page.
+  let cls = 0;
 
-// Create the PerformanceObserver instance.
-const po = new PerformanceObserver(entryList => {
-  for (const entry of entryList.getEntries()) {
-    // Only count layout shifts without recent user input.
-    if (!entry.hadRecentInput) {
-      cls += entry.value;
-      // Send the CLS to the background page
-      chrome.runtime.sendMessage({ result: cls.toFixed(3) });
-      console.log("CLS:", cls);
+  // Create the PerformanceObserver instance.
+  const po = new PerformanceObserver((entryList) => {
+    for (const entry of entryList.getEntries()) {
+      // Only count layout shifts without recent user input.
+      if (!entry.hadRecentInput) {
+        cls += entry.value;
+        // Send the CLS to the background page
+        chrome.runtime.sendMessage({ result: cls.toFixed(3) });
+        console.log("CLS:", cls);
+      }
     }
-  }
-});
+  });
 
-po.observe({ type: "layout-shift", buffered: true });
+  po.observe({ type: "layout-shift", buffered: true });
+}
